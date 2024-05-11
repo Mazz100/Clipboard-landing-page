8a1c409ffb401cc4ee5541b7f9bc1f0ddfbedddc
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-05-11 14:57:04 +0300

    Mobile Layout update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    backgroundColor:{
+      "background-iOS": "#26baa4",
+      "background-Mac": "#6173ff",
+    },
+    
+    /*boxShadowColor:{
+      "iOS-shadow":
+    }*/
+
+
     extend: {},
   },
   plugins: [],
