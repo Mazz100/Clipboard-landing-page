989e6a50d5026b930df2d6f7dbf750560a455e1d
Mazen Hassan <95134348+Mazz100@users.noreply.github.com>
2024-05-12 14:08:29 +0300

    Desktop Layout and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,36 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:
-    [
-      "./src/**/*.{html,js}",
-      './index.html',
-    ],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
     backgroundColor: {
       "background-iOS": "#26baa4",
       "background-Mac": "#6173ff",
     },
 
+    extend: {
+      fontFamily: {
+        "Bai-Jamjuree": ["Bai Jamjuree", "sans-serif"],
+      },
 
-    extend: {},
+      boxShadow: {
+        "iOS-shadow": "0 4px 10px  rgba(38, 186, 164, 0.4)",
+        "Mac-shadow": "0 4px 10px rgba(97, 115, 255, 0.4)",
+      },
+
+      borderColor: {
+        "iOS-border-color": "#49a896",
+        "Mac-border-color": "#727dd6",
+      },
+
+      backgroundColor: {
+        "hover-iOS": "#5dccbb",
+        "hover-Mac": "#8896ff",
+      },
+
+      colors: {
+        "hover-links": "#26baa4",
+      }
+    },
   },
   plugins: [],
-}
-
+};
